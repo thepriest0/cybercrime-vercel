e844a1f4beaaf91ebb5e02251fe7335b533ef5a2
thepriest0 <thereviewmr@gmail.com>
2024-06-23 08:54:22 +0100

    db

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://cybercrime_user:iVk5T18JBYdeAmHLvLn32nWMnr4FZlBq@dpg-cpql41aj1k6c73bic3b0-a/cybercrime')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key'
